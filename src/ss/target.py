d259a4410eb62f2fb4a435e85209e267a083a1b4
Milix-M <tujikou12dh@gmail.com>
2023-10-11 02:08:36 +0900

    古い形式のスクリーンショット名に対応

--- src/ss/target.py
+++ src/ss/target.py
@@ -36,13 +36,17 @@ def get_target_ss(path: str) -> list:
     # example: VRChat_2022-11-14_21-35-14.256_1920x1080.png
     pattern = r'VRChat_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}_\d{4}x\d{4}.png'
 
+    # old naming example: VRChat_7680x4320_2022-10-06_22-50-52.350.png
+    pattern_old = r'VRChat_\d{4}x\d{4}_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}.png'
+
     ss = []
 
     # ディレクトリ内のファイルを検索
     for dir in path:
         for root, dirs, files in os.walk(dir):
             for file_name in files:
-                if re.match(pattern, file_name):
+                #最新形式と古い命名形式両方チェックしてssリストにappend
+                if re.match(pattern, file_name) or re.match(pattern_old, file_name):
                     ss.append(os.path.join(root, file_name))
 
     return ss
