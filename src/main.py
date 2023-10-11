9a40063ca3a3b46989cfb6890bf4954822c0599d
Milix-M <tujikou12dh@gmail.com>
2023-10-11 23:09:28 +0900

    日付分けするか選択するようにする

--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ from ss import target, ss_mnj
 with open("./settings.json") as f:
     settings = json.load(f)
 
-sort_target = target.get_target_ss(target.get_target_dir(settings["ss_path"]))
+print("スクリーンショットを日付分けしますか？: y/n")
+choice = input("")
 
-ss_mnj.copy_ss(sort_target, settings["ss_path"])
+if choice == "y":
+    sort_target = target.get_target_ss(target.get_target_dir(settings["ss_path"]))
+
+    ss_mnj.copy_ss(sort_target, settings["ss_path"])
+else:
+    print("処理を中止します")
