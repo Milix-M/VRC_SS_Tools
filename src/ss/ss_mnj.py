62e55ff2f17e15e677ca7072595607a3b38d7d03
Milix-M <tujikou12dh@gmail.com>
2023-10-11 22:37:24 +0900

    merge function

--- src/ss/ss_mnj.py
+++ src/ss/ss_mnj.py
@@ -2,16 +2,18 @@ import os
 import re
 import shutil
 
-def make_days_dir(sss: list, path: str) -> None:
-    """日付ごとにフォルダを作成します
-    SSの名前から日付を検出し日付ごとに作成
+
+def copy_ss(ss_paths: list, path: str) -> None:
+    """日付分けごとのディレクトリを作成してSSをコピーします
 
     Args:
-        sss (list): 日付ごとのフォルダを作成するスクリーンショットのパスを指定
-        path (str): 日付ごとのフォルダを作成するディレクトリを指定
+        ss_paths (list): 対象となるSSのパス(list)
+        path (str): 日付分けされたSSを作成するディレクトリを指定
     """
+
     extracted_text = []
-    for ss in sss:
+
+    for ss in ss_paths:
         m = re.search(r'(\d{4}-([0-1][0-9])-([0-3][0-9]))', ss)
         if m:
             extracted_text.append(m.group())
@@ -23,13 +25,6 @@ def make_days_dir(sss: list, path: str) -> None:
         if not os.path.exists(os.path.join(path, make)):
             os.makedirs(os.path.join(path, make))
 
-def copy_ss(ss_paths: list, path: str) -> None:
-    """スクリーンショットをコピーします
-
-    Args:
-        ss_path (list): スクリーンショットのパスを指定
-        path (str): 日付ごとのフォルダが存在するディレクトリのパスを指定
-    """
     for ss in ss_paths:
         day = re.search(r'(\d{4}-([0-1][0-9])-([0-3][0-9]))', ss)
         shutil.copy(os.path.join(path, ss), os.path.join(path, day.group()))
