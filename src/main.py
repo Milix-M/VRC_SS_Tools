ba95a4c29c5b9a026ecd2978025299c569c2a025
Milix-M <tujikou12dh@gmail.com>
2023-10-11 22:30:31 +0900

    スクリーンショットをコピーできるようにした

--- src/main.py
+++ src/main.py
@@ -10,3 +10,5 @@ with open("./settings.json") as f:
 sort_target = target.get_target_ss(target.get_target_dir(settings["ss_path"]))
 
 ss_mnj.make_days_dir(sort_target, settings["ss_path"])
+
+ss_mnj.copy_ss(sort_target, settings["ss_path"])
