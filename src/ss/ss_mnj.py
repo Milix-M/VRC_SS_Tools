47491b151160201a87e5c73b4ec333f4fdeb9b9b
Milix-M <tujikou12dh@gmail.com>
2023-10-16 21:03:27 +0900

    使用していないコードを削除

--- src/ss/ss_mnj.py
+++ src/ss/ss_mnj.py
@@ -50,9 +50,6 @@ def copy_ss(ss_paths: list, path: str, date_line: int) -> None:
         if not os.path.exists(os.path.join(path, make)):
             os.makedirs(os.path.join(path, make))
 
-    # for ss in ss_paths:
-    #     day = re.search(r'(\d{4}-([0-1][0-9])-([0-3][0-9]))', ss)
-    #     shutil.copy(os.path.join(path, ss), os.path.join(path, day.group()))
 
     #date_sspath_dictを元にSSをコピー
     for ss_path, putting_dir in date_sspath_dict.items():
