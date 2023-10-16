6ad7986031f9df6182b42d677bc8fd38989004f0
Milix-M <tujikou12dh@gmail.com>
2023-10-16 22:13:56 +0900

    自動的に日付分けするように変更

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 #ソートターゲットディレクトリ走査用モジュール
 from ss import target, ss_mnj
@@ -7,12 +8,7 @@ from ss import target, ss_mnj
 with open("./settings.json") as f:
     settings = json.load(f)
 
-print("スクリーンショットを日付分けしますか？: y/n")
-choice = input("")
-
-if choice == "y":
+while True:
+    time.sleep(10)
     sort_target = target.get_target_ss(target.get_target_dir(settings["ss_path"]))
-
     ss_mnj.copy_ss(sort_target, settings["ss_path"], settings["date_line"])
-else:
-    print("処理を中止します")
