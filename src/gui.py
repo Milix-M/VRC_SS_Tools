21881a6a0ee437cfa01b7bcf667cc897f5ece60c
Milix-M <tujikou12dh@gmail.com>
2023-10-20 14:13:56 +0900

    move Kivy Files & add Button clicked func

--- src/gui.py
+++ src/gui.py
@@ -6,14 +6,29 @@ Config.set('graphics', 'height', '200')
 Config.set('graphics', 'resizable', 0)
 
 import japanize_kivy
+import json
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 
 
 class TextWidget(Widget):
     text = StringProperty()
+    input = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(TextWidget, self).__init__(**kwargs)
+
+    def buttonClicked(self):
+        with open("./settings.json") as f:
+            settings = json.load(f)
+
+        settings["ss_path"] = self.input.text
+
+        with open("./settings.json", "w") as f:
+            json.dump(settings, f, indent=2)
+
 
 
 class TestApp(App):
